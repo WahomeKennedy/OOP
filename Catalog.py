a627f1b99b7984eb2349642a477c3ee3482f0734
WahKen <120699734+WahomeKennedy@users.noreply.github.com>
2023-03-02 20:29:14 +0300

    Update

--- Catalog.py
+++ Catalog.py
@@ -1,7 +1,8 @@
 from Course import Course
-id = {
-    1: "Kenito",
-    2: "Lenny",
-    3: "William",
-    4: "Billy"
-}
+
+class inherit(Course):
+    pass
+
+    var = Course()
+var.id
+
--- Person.py
+++ Person.py
@@ -1,10 +1,22 @@
 # This class represents a person, with attributes for a name and a social security number
 class Person:
-    def __init__(self, name, social_security_number):
+    def __init__(self, name, social_security_number, staff_member, has_supervisor,student):
         self.name = name
         self.social_security_number = social_security_number
+        self.staff_member = staff_member
+        self.has_supervisor = has_supervisor
     
     # This class represents person who is a staff member and has a supervisor
-    def staff(self,staff_member):
-        self.staff_member = staff_member
-        
+class Staff(Person):
+    pass
+
+case1 = Person()
+    
+# This subclass represents a person who is a student that takes courses at the university
+class Student2(Person, Course, Loan, Student1):
+    pass
+
+case2 = Person()
+case3 = Course()
+case4 = Loan()
+case5 = Student1()
